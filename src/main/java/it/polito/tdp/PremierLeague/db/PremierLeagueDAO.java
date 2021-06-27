62963668027792002cf52f831d0e6b3b1d8dae15
Lara <s258856@studenti.polito.it>
2021-06-27 13:20:44 +0200

    versione 1

--- src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
+++ src/main/java/it/polito/tdp/PremierLeague/db/PremierLeagueDAO.java
@@ -6,15 +6,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.PremierLeague.model.Action;
 import it.polito.tdp.PremierLeague.model.Player;
 
 public class PremierLeagueDAO {
 	
-	public List<Player> listAllPlayers(){
+	
+	
+	public void listAllPlayers(Map <Integer, Player> idMap){
 		String sql = "SELECT * FROM Players";
-		List<Player> result = new ArrayList<Player>();
+		//List<Player> result = new ArrayList<Player>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
@@ -22,9 +25,70 @@ public class PremierLeagueDAO {
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
 
+				if(!idMap.containsKey(res.getInt("PlayerID"))) {
 				Player player = new Player(res.getInt("PlayerID"), res.getString("Name"));
 				
-				result.add(player);
+				//result.add(player);
+				idMap.put(player.getPlayerID(), player);
+			}
+			}
+			conn.close();
+			//return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			//return null;
+		}
+	}
+	
+	public void listAllActions(Map<Integer, Action> idMapAzioni){
+		String sql = "SELECT * FROM Actions";
+		//List<Action> result = new ArrayList<Action>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				if(!idMapAzioni.containsKey(res.getInt("MatchID"))) {
+				Action action = new Action(res.getInt("PlayerID"),res.getInt("MatchID"),res.getInt("TeamID"),res.getInt("Starts"),res.getInt("Goals"),
+						res.getInt("TimePlayed"),res.getInt("RedCards"),res.getInt("YellowCards"),res.getInt("TotalSuccessfulPassesAll"),res.getInt("totalUnsuccessfulPassesAll"),
+						res.getInt("Assists"),res.getInt("TotalFoulsConceded"),res.getInt("Offsides"));
+				
+				idMapAzioni.put(res.getInt("MatchID"), action);
+				//result.add(action);
+			}
+			}
+			conn.close();
+			//return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			//return null;
+		}
+	}
+	
+	public List <Player> getVertici(int goal, Map <Integer, Player> idMap){
+		String sql = "SELECT p.PlayerID AS giocatore, AVG(a.Goals) AS media "
+				+ "FROM players p, actions a "
+				+ "WHERE p.PlayerID = a.PlayerID "
+				+ "GROUP BY a.PlayerID "
+				+ "HAVING AVG(a.Goals)> ?";
+		
+		List<Player> result = new ArrayList<>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, goal);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Player p = idMap.get(res.getInt("giocatore"));
+				result.add(p);
+			
+				
 			}
 			conn.close();
 			return result;
@@ -35,21 +99,28 @@ public class PremierLeagueDAO {
 		}
 	}
 	
-	public List<Action> listAllActions(){
-		String sql = "SELECT * FROM Actions";
-		List<Action> result = new ArrayList<Action>();
+	public List <Adiacenza> getAdiacenze(Map <Integer, Player> idMap,Map <Integer, Action> idMapAzioni, int goal){
+		
+		String sql = "SELECT a1.PlayerID, a2.PlayerID, AVG(a1.Goals) , AVG(a2.Goals) "
+				+ "FROM players p1, actions a1, players p2, actions a2 "
+				+ "WHERE p1.PlayerID = a1.PlayerID AND p2.PlayerID = a2.PlayerID AND a1.TeamID> a2.TeamID "
+				+ "AND a1.MatchID=a2.MatchID AND a1.`Starts`= 1 AND a2.`Starts`=1 AND p1.PlayerID>p2.PlayerID "
+				+ "GROUP BY a1.PlayerID, a2.PlayerID "
+				+ "HAVING AVG(a1.Goals) > 0.5 AND AVG(a2.Goals)>= AVG(a1.Goals)";
+		
+		List<Adiacenza> result = new ArrayList<>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, goal);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
 
-				Action action = new Action(res.getInt("PlayerID"),res.getInt("MatchID"),res.getInt("TeamID"),res.getInt("Starts"),res.getInt("Goals"),
-						res.getInt("TimePlayed"),res.getInt("RedCards"),res.getInt("YellowCards"),res.getInt("TotalSuccessfulPassesAll"),res.getInt("totalUnsuccessfulPassesAll"),
-						res.getInt("Assists"),res.getInt("TotalFoulsConceded"),res.getInt("Offsides"));
+				Player p = idMap.get(res.getInt("giocatore"));
+				result.add(p);
+			
 				
-				result.add(action);
 			}
 			conn.close();
 			return result;
@@ -59,4 +130,6 @@ public class PremierLeagueDAO {
 			return null;
 		}
 	}
+	
+	
 }
--- src/main/java/it/polito/tdp/PremierLeague/model/Model.java
+++ src/main/java/it/polito/tdp/PremierLeague/model/Model.java
@@ -1,5 +1,47 @@
 package it.polito.tdp.PremierLeague.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.PremierLeague.db.PremierLeagueDAO;
+
 public class Model {
 
+	private SimpleDirectedWeightedGraph <Player, DefaultWeightedEdge> grafo;
+	private PremierLeagueDAO dao;
+	private Map <Integer, Player> idMap;
+	private Map <Integer, Action> idMapAzioni;
+	
+	public Model() {
+		dao = new PremierLeagueDAO ();
+		idMap = new HashMap<>();
+		idMapAzioni = new HashMap<>();
+		dao.listAllPlayers(idMap);
+		dao.listAllActions(idMapAzioni);
+		
+		
+	}
+	
+	public void creaGrafo(int goal) {
+		grafo = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		 //aggiungo vertici
+		Graphs.addAllVertices(grafo,dao.getVertici(goal, idMap));
+		
+		//aggiungo archi
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
 }
